a1913f804ebd5b04ac5e28fae27ba6f6ff0b54c5
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-14 21:37:03 +0300

    7-8

--- LoginWeb2/src/main/java/ohtu/Tester.java
+++ LoginWeb2/src/main/java/ohtu/Tester.java
@@ -14,18 +14,45 @@ public class Tester {
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click(); 
         
-        System.out.println("==");
+        System.out.println("==\nepäonnistunut kirjautuminen: oikea käyttäjätunnus, väärä salasana\n==");
         
         System.out.println( driver.getPageSource() );
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        element.sendKeys("akke");
         element = driver.findElement(By.name("login"));
         element.submit();
         
-        System.out.println("==");
+        System.out.println("==\nepäonnistunut kirjautuminen: ei-olemassaoleva käyttäjätunnus\n==");
         System.out.println( driver.getPageSource() );
-        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekk");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        System.out.println("==\nuuden käyttäjätunnuksen luominen\n==");
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        System.out.println( driver.getPageSource() );
+        String password = "matti1juhani";
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("abcdef");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("add"));
+        element.submit();
+        System.out.println("==\nuuden käyttäjätunnuksen luomisen jälkeen tapahtuva ulkoskirjautuminen sovelluksesta\n==");
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        System.out.println( driver.getPageSource() );
     }
 }
